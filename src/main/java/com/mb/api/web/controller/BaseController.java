8eb365803a7264c15462a20a3e121496daa94184
= <=>
2022-11-28 12:01:31 +0530

    base controller updated for cross origins!

--- src/main/java/com/mb/api/web/controller/BaseController.java
+++ src/main/java/com/mb/api/web/controller/BaseController.java
@@ -1,11 +1,13 @@
 package com.mb.api.web.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import static com.mb.api.business.constant.GenericConstant.BASE_URL;
 
-@RestController
+
 @RequestMapping(BASE_URL)
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class BaseController
 {
 
